701559fddb48f5f8abeb77699a8299616fbaac1d
Victor Oliveira <victorphellyppe@hotmail.com>
2021-06-10 16:38:14 -0300

    Requisições http em api publica, requisições, get, get all, update, delete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private apiService:ApiService) {
+    // this.createDados();
+    // this.readData();
+    this.readAll();
+    // this.updateData();
+    this.deleteDado();
+  }
 
-  constructor() {}
+  createDados(){
+    const data:any = {
+      title: 'Victor',
+      body: 'Estou testando um app, com uma api publica.',
+      userId: 1,
+    };
 
+    this.apiService.createData(data).subscribe(data => {
+      console.log(data);
+    });
+    
+  }
+  readData(){
+    this.apiService.readData().subscribe(data => {
+      console.log(data);
+    });
+  }
+  readAll(){
+    this.apiService.readData().subscribe(dados => {
+      console.log(dados);
+    })
+  }
+
+  updateData(){
+    const data: any = {
+      id:1,
+      title: 'Oliveira Victor',
+    body: 'Tamo no github',
+    userId: 11,
+    };
+    this.apiService.updateData(data).subscribe(data => { console.log(data);})
+  }
+
+  deleteDado(){
+    return this.apiService.deleteData().subscribe(data => { console.log('Deletado ', data); })
+
+  }
 }
